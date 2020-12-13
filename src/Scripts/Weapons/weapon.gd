ef2d3d554ff5b44a197553cd3d6c3932a714f9e3
J.B Stepan <tonymoooon543@gmail.com>
2020-12-13 14:46:56 -0600

    Update weapon.gd

--- src/Scripts/Weapons/weapon.gd
+++ src/Scripts/Weapons/weapon.gd
@@ -10,60 +10,21 @@
 extends Node;
 
 class Gun:
-	# Basic
 	var owner : Node;
 	var name : String;
-	var firerate : float;
-	var bullets : int;
+	var fire_rate : float;
+	var clip_size : int;
 	var ammo : int;
-	var max_bullets : int;
-	var damage : int;
-	var reload_speed : float;
 	
-	# NOTE(@JB Stepan): Not going to do these right now
-	# Triside Things
-	var type : int;
-	var is_sniper : bool = false;
-	
-	# NOTE(@JB Steapn): I know im going againest my own words here, but I dont want to confuse the engine or myself
-	func _init(Owner : Node, Name : String, Firerate : float, Bullets : int, Ammo : int, MaxBullets : int, Damage : float, ReloadSpeed : float)->void:
+	func _init(Owner, Name, FireRate, ClipSize, Ammo):
 		self.owner = Owner;
 		self.name = Name;
-		self.firerate = Firerate;
-		self.bullets = Bullets;
+		self.fire_rate = FireRate;
+		self.clip_size = ClipSize;
 		self.ammo = Ammo;
-		self.max_bullets = MaxBullets;
-		self.damage = Damage;
-		self.reload_speed = ReloadSpeed;
-		
-	var can_fire : bool = false;
-	
-	var mesh = owner.get_node("{}".format([name], "{}"));
 	
 	func _draw()->void:
-		if not mesh.visable: #TODO: Anims
-			mesh.visable = true;
-			print("Weapon drawn");
-			can_fire = true;
-
+		print("Weapon drawn")
+	
 	func _hide()->void:
-		if mesh.visable:
-			mesh.visable = false
-			print("Weapon hidden");
-			can_fire = false;
-	
-	#NOTE(@JB Stepan): Im doing the same thing here, sue me
-	func _sprint(sprint, _delta)->void:
-		pass
-	
-	func _fire(_delta)->void:
-		pass
-		
-	func _reload(_delta)->void:
-		pass
-	
-	func _aim(_delta)->void:
-		pass
-		
-	func _change(_delta)->void:
-		pass
+		print("Weapon hidden")
