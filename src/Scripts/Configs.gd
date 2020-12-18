cf8b4d8a38a1f662aa8a31ccdf42e0829de47f7b
J.B Stepan <tonymoooon543@gmail.com>
2020-12-18 17:59:40 -0600

    Update Configs.gd

--- src/Scripts/Configs.gd
+++ src/Scripts/Configs.gd
@@ -1,3 +1,12 @@
+#
+# Configs.gd
+# ------------------
+# Part of the Triside
+# Copyright (c) 2020 JB Stepan. All Rights Reserved
+# Licensed under License.txt. License.txt for more info.
+#
+# TODO: 
+
 extends Node
 
 func _enter_tree():
