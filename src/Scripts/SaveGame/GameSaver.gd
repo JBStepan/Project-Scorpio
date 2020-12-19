2ebe0a45f878f4ef6f978ad96231bf34b75250ba
J.B Stepan <tonymoooon543@gmail.com>
2020-12-18 18:00:12 -0600

    Update GameSaver.gd

--- src/Scripts/SaveGame/GameSaver.gd
+++ src/Scripts/SaveGame/GameSaver.gd
@@ -1,3 +1,12 @@
+#
+# GameSaver.gd
+# ------------------
+# Part of the Triside
+# Copyright (c) 2020 JB Stepan. All Rights Reserved
+# Licensed under License.txt. License.txt for more info.
+#
+# TODO: 
+
 extends Node
 
 const SAVEGAME = preload("res://Scripts/SaveGame/SaveGame.gd");
