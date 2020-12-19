cddd43e1ce588d3eb2a786dfbedb4720668c4ca9
J.B Stepan <tonymoooon543@gmail.com>
2020-12-18 18:00:26 -0600

    Update SaveGame.gd

--- src/Scripts/SaveGame/SaveGame.gd
+++ src/Scripts/SaveGame/SaveGame.gd
@@ -1,3 +1,12 @@
+#
+# SaveGame.gd
+# ------------------
+# Part of the Triside
+# Copyright (c) 2020 JB Stepan. All Rights Reserved
+# Licensed under License.txt. License.txt for more info.
+#
+# TODO: 
+
 extends Resource
 class_name SaveGame
 
