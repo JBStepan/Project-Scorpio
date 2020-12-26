85bb9f99a2fcbea62b090654e5ec425e2d7b822e
J.B Stepan <tonymoooon543@gmail.com>
2020-12-26 16:57:52 -0600

    Build system

---
+++ src/build.bat
@@ -0,0 +1,5 @@
+@echo off
+
+echo Building Triside!
+C:\Games\Godot_v3.2.2-stable_win64.exe --path C:\Games\Triside\src\project.godot --export "Windows Main" C:\Games\Triside\builds\12-26-2020\triside.exe
+pause 
