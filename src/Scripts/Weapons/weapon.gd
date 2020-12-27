4a499d582f441b48669dbd6402fbd3ba4a27eb92
J.B Stepan <tonymoooon543@gmail.com>
2020-12-27 12:48:16 -0600

    Fixed Errors when exporting

--- src/Scripts/Weapons/weapon.gd
+++ src/Scripts/Weapons/weapon.gd
@@ -7,11 +7,13 @@
 #
 # TODO: Work on hold to fire
 
-extends Node
+extends Spatial 
 
-class_name Weapon
+class_name Gun
 
-export var debug_name : String
+# Base weapon things
+export var editor_id : String
+export var use_weapon_file : bool = true;
 export(String, FILE, '*.weapon') var weapon_file
 export var fire_rate : float;
 export var hold_fire_rate : float;
@@ -20,6 +22,11 @@ export var reload_rate : float;
 export var damage : float;
 export var can_hold : bool;
 export var raycast_path : NodePath;
+
+# Aiming
+export var default_pos : Vector3;
+export var aim_pos : Vector3;
+export var aim_speed : float;
 
 # Enums
 enum WeaponType { RIFLE, SNIPER, SMG, LMG, PISTOL }
@@ -34,10 +41,11 @@ export(String, MULTILINE) var weapon_desc
 var current_ammo = 0;
 var can_fire = true;
 var reloading = false;
+var is_aiming = false;
 
 var raycast : RayCast;
 
-func _ready():
+func _ready()->void:
 	if weapon_file != "":
 		_read_weapon_file(weapon_file)
 	else:
@@ -46,7 +54,7 @@ func _ready():
 	current_ammo = clip_size;
 	raycast = get_node(raycast_path);
 	
-func _process_gun():
+func _process_gun()->void:
 	if Input.is_action_just_pressed("fire") and can_fire:
 		if current_ammo > 0 and not reloading:
 			fire();
@@ -55,8 +63,14 @@ func _process_gun():
 	
 	if Input.is_action_just_pressed("reload") and not reloading and current_ammo < clip_size:
 		reload();
+	
+	# Aiming
+	if Input.is_action_pressed("aim"):
+		transform.origin = transform.origin.linear_interpolate(aim_pos, aim_speed)
+	else:
+		transform.origin = transform.origin.linear_interpolate(default_pos, aim_speed)
 
-func check_collision():
+func check_collision()->void:
 	if raycast.is_colliding():
 		var collider = raycast.get_collider();
 		if collider.is_in_group("Enemies"):
@@ -64,38 +78,44 @@ func check_collision():
 		else:
 			print("Hit Nothing!");
 
-func fire():
+func fire()->void:
 	print("Fired weapon " + weapon_name);
 	can_fire = false;
 	current_ammo -= 1;
 	yield(get_tree().create_timer(fire_rate), "timeout");
 	can_fire = true
 
-func reload():
+func reload()->void:
 	reloading = true;
 	print("Reloading weapon " + weapon_name)
 	yield(get_tree().create_timer(reload_rate), "timeout");
 	current_ammo = clip_size;
-	print("Done reloading weapon" + weapon_name)
+	print("Done reloading weapon " + weapon_name)
 	reloading = false;
 
 # Reads the .weapon file given to it.
-func _read_weapon_file(weaponfile):
-	var config = ConfigFile.new()
-	var err = config.load(weaponfile)
+func _read_weapon_file(weaponfile)->void:
+	var config = ConfigFile.new();
+	var err = config.load(weaponfile);
 	
 	var weapon_file_type = config.get_value("Weapon", "type");
 	
-	# If type in the .weapon file is gun, then read the variables in the file, else, print an error
-	if weapon_file_type == "gun":
-		if err == OK: # If not, something went wrong with the file loading
-			weapon_name = config.get_value("Weapon", "name");
-			fire_rate = config.get_value("Weapon", "firerate");
-			hold_fire_rate = config.get_value("Weapon", "holdfirerate");
-			clip_size = config.get_value("Weapon", "clipsize");
-			reload_rate = config.get_value("Weapon", "reloadrate");
-			damage = config.get_value("Weapon", "damage");
+	if use_weapon_file == true:
+		# If type in the .weapon file is gun, then read the variables in the file, else, print an error
+		if weapon_file_type == "gun":
+			if err == OK: # If not, something went wrong with the file loading
+				weapon_name = config.get_value("Weapon", "name");
+				fire_rate = config.get_value("Weapon", "firerate");
+				hold_fire_rate = config.get_value("Weapon", "holdfirerate");
+				clip_size = config.get_value("Weapon", "clipsize");
+				reload_rate = config.get_value("Weapon", "reloadrate");
+				damage = config.get_value("Weapon", "damage");
+				default_pos = config.get_value("Weapon", "default_pos");
+				aim_pos = config.get_value("Weapon", "aim_pos");
+				aim_speed = config.get_value("Weapon", "aim_speed");
+			else:
+				print("Something went wrong loading file " + weapon_file);
 		else:
-			print("Something went wrong loading file " + weapon_file)
+			print("Weapon type is not accepted!");
 	else:
-		print("Weapon type is not accepted!")
+		print("Not using .weapon file")
--- src/build.bat
+++ src/build.bat
@@ -1,5 +1,6 @@
 @echo off
 
 echo Building Triside!
-C:\Games\Godot_v3.2.2-stable_win64.exe --path C:\Games\Triside\src\project.godot --export "Windows Main" C:\Games\Triside\builds\12-26-2020\triside.exe
-pause 
+C:\Games\Godot_v3.2.2-stable_win64.exe --no-window --path C:\Games\Triside\src\project.godot --export "Windows Main" C:\Games\Triside\builds\12-27-2020\triside.exe
+echo Done!
+pause
