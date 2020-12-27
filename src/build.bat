fd60ed5fb84dc21ef97bd62a8af90b301f47d2de
J.B Stepan <tonymoooon543@gmail.com>
2020-12-27 14:42:22 -0600

    1

--- src/build.bat
+++ src/build.bat
@@ -1,11 +1,6 @@
 @echo off
 
-set godot_path = C:\Games\Godot_v3.2.2-stable_win64.exe
-set proj_path = C:\Games\Triside\src\project.godot
-set export_preset = "Windows Main"
-set export_path = C:\Games\Triside\builds\12-27-2020\triside.exe
-
 echo Building Triside!
-%godot% --path %proj_path% --export %export_preset% %export_path%
+C:\Games\Godot_v3.2.2-stable_win64.exe --windowed --path C:\Games\Triside\src\project.godot --export "Windows Main" C:\Games\Triside\builds\12-27-2020\triside.exe
 echo Done!
 pause
